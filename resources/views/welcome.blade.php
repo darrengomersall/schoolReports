42c24c48317cdb6673b3c93ab4f5ab1952a5b72e
Darren Gomersall <darren@cuberoute.co.za>
2018-01-08 15:51:08 +0200

    js and css files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Darling College
                 </div>
 
                 <div class="links">
