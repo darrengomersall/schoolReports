fb3bdb2ff56a91a394ab2f9784c04f833029712f
Darren Gomersall <darren@cuberoute.co.za>
2018-01-09 09:07:20 +0200

    navbar updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-sm-12" style="">
+            <div class="col-sm-12">
                 <h1>Welcome back, {{ Auth::user()->name }}</h1>
             </div>
         </div>
