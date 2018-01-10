f728689dae6084562bbe13372e2bd5e26004d2ad
Darren Gomersall <darren@cuberoute.co.za>
2018-01-10 19:29:41 +0200

    pupil migration and model

---
+++ database/migrations/2018_01_09_182313_create_pupils_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePupilsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pupils', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('cemis_num');
+            $table->date('dob');
+            $table->string('language');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pupils');
+    }
+}
