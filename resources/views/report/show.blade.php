1df40b08d8b069b2fb9471367b849b8e203cd5ca
Darren Gomersall <darren@cuberoute.co.za>
2018-01-14 08:02:11 +0200

    add return to pupil button

--- resources/views/report/show.blade.php
+++ resources/views/report/show.blade.php
@@ -1,10 +1,14 @@
 @extends('partials.base')
     @section('content')
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-lg-10">
                 <h1>
                     Report for: <span>{{ $report->pupil->firstname . " " . $report->pupil->lastname }}</span>
                 </h1>
             </div>
+            <div class="col-lg-2">
+               <a href="/pupil/view/{{ $report->pupil->id }}" class="btn btn-dark">Return to Pupil</a>
+            </div>
+
         </div>
     @endsection
