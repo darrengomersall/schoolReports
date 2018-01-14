60e9cff90c3f7d48f122a560c65d7b325bba5091
Darren Gomersall <darren@cuberoute.co.za>
2018-01-14 08:13:40 +0200

    subject group model + migration

---
+++ app/SubjectGroup.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubjectGroup extends Model
+{
+    //
+    public function grade ()
+    {
+        return $this->belongsTo('App\Grade');
+    }
+
+    public function subjects ()
+    {
+
+    }
+
+}
